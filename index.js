b115fc8648e455189917284acabea7fa154fa9c3
Poryx <61403237+Poryx@users.noreply.github.com>
2020-06-06 19:29:54 -0600

    1

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Client, MessageEmbed } = require('discord.js');
+const client = new Client();
+
+
+client.on('ready', () => {
+  console.log(`Bot ready as: ${client.user.tag}!`);
+  console.log(`Bot status: ${client.user.presence.status}`)
+});
+
+
+client.on('message', message => {
+ 
+  var posi = ["yes", "no", "maybe"]
+  var aleatorio = Math.floor(Math.random()*(posi.length))
+  
+
+  if (message.content.startsWith(`=true`)) {
+    message.reply(posi[aleatorio])
+  }
+
+  if(message.content === "=help"){
+    const embed = new MessageEmbed()
+    .setTitle("HELP")
+    .setDescription("*Use: =true (your question) for know the true*")
+    .setFooter("Dm poryx#3975 for internal support")
+    .setColor("RANDOM")
+    .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    //.setThumbnail("")
+    message.channel.send(embed)
+  }
+
+});
+
+client.login('NzEzODY4OTUxNjIwNDE5Njc1.XtwZpA.RajqBSNRiJ-V6NzfaZFTg7MsTR8');ç
